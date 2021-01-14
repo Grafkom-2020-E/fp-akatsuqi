89dd98c8a25ca5557d8ed2e8932d3fc7df8adb58
exceldeo <52096462+exceldeo@users.noreply.github.com>
2021-01-14 22:12:21 +0700

    menambahkan informasi api

--- assets/main.js
+++ assets/main.js
@@ -202,6 +202,8 @@ class Main {
             10
         );
         const e6 = new entity.Entity();
+        e6.setType('rusa');
+        e6.setId('5ffff56abfb6bacaaf78df52');
     
         e6.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -227,6 +229,8 @@ class Main {
             67
         );
         const e1 = new entity.Entity();
+        e1.setType('Trenggiling');
+        e1.setId('5ffff743bfb6bacaaf78df53');
     
         e1.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -252,6 +256,8 @@ class Main {
             108
         );
         const e7 = new entity.Entity();
+        e7.setType('Kura-kura');
+        e7.setId('5ffff81dbfb6bacaaf78df54');
     
         e7.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -277,6 +283,8 @@ class Main {
             150
         );
         const e2 = new entity.Entity();
+        e2.setType('Kuda');
+        e2.setId('5ffff8b6bfb6bacaaf78df55');
     
         e2.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -302,6 +310,8 @@ class Main {
             232
         );
         const e3 = new entity.Entity();
+        e3.setType('Jerapah');
+        e3.setId('5ffff95bbfb6bacaaf78df56');
     
         e3.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -327,6 +337,8 @@ class Main {
             255
         );
         const e4 = new entity.Entity();
+        e4.setType('Gajah');
+        e4.setId('5ffff1ccbfb6bacaaf78df51');
     
         e4.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -352,6 +364,8 @@ class Main {
             170
         );
         const e5 = new entity.Entity();
+        e5.setType('Harimau');
+        e5.setId('5ffffa09bfb6bacaaf78df57');
     
         e5.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -377,6 +391,8 @@ class Main {
             120
         );
         const e8 = new entity.Entity();
+        e8.setType('Beruang');
+        e8.setId('5ffffaae1fc2ecf90c474676');
     
         e8.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
@@ -402,6 +418,8 @@ class Main {
             40
         );
         const e9 = new entity.Entity();
+        e8.setType('Gorila');
+        e8.setId('5ffffbb91fc2ecf90c474677');
     
         e9.addComponent(new gltf.StaticModelComponent({
             scene: this._scene,
